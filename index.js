f7b6e3027184f75390cca617eceaead26d270c9c
theonlysandman <sandy@theonlysandman.ca>
2025-11-16 13:23:21 -0500

    Test of auto deployment.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/healthz", (req, res) => res.send("OK"));
+app.get("/healthz", (req, res) => res.send("OK Sandy"));
 app.listen(port, () => console.log(`Server running on port ${port}`));
